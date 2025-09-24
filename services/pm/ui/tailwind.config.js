2b46ca6e7965d31ddbfab7ac83df73e709e743d1
dattamlong <ndat9124@gmail.com>
2025-09-25 03:26:50 +0700

    fix: package ui css

--- services/pm/ui/tailwind.config.js
+++ services/pm/ui/tailwind.config.js
@@ -1,5 +1,15 @@
-
+const path = require("path");
 const config = require("@unified-teamspace/tailwind-config/tailwind.config.js");
-config.content.files = ["./src/**/*.{js,ts,jsx,tsx}"];
+
+const rootDir = path.join(__dirname, "..", "..", "..", "..");
+
+config.content.files = [
+  ...new Set([
+    ...(config.content?.files ?? []),
+    "./src/**/*.{js,ts,jsx,tsx}",
+    path.join(rootDir, "packages/design-system/ui/src/**/*.{js,ts,jsx,tsx}"),
+    path.join(rootDir, "packages/design-system/ui/dist/**/*.{js,jsx,ts,tsx,mjs}"),
+  ]),
+];
 
 module.exports = config;
